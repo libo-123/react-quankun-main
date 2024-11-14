6764664f85d750e025a70d463143d79d04e95cc5
libo <15821258946@163.com>
2024-11-15 00:00:20 +0800

    fix: button

--- src/views/main/index.tsx
+++ src/views/main/index.tsx
@@ -1,16 +1,17 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import { Button } from "antd";
+import { useNavigate } from "react-router-dom";
 
 const Main = () => {
+  const navigate = useNavigate();
   return (
     <div>
-        <h1>顶部搜索兰姆</h1>
-        <div className="content">
-            跳转区域
-            <Link to="/anchor">我的页面</Link>
-        </div>
+      <h1>顶部搜索兰姆</h1>
+      <div className="content">
+        跳转区域
+        <Button onClick={() => navigate("/anchor")}>我的页面</Button>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Main
+export default Main;
