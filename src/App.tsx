db515d5780408bdbf2506d3c32e56bc254f1cc14
libo <15821258946@163.com>
2024-11-25 23:55:21 +0800

    feat: 解决路由不加载问题

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,17 @@
-import { Route, Routes } from "react-router-dom";
-import Sub from "./views/sub";
-import Main from "./views/main";
+import { Route, Routes, useLocation, useNavigate } from 'react-router-dom';
+import Sub from './views/sub';
+import Main from './views/main';
 
 import '@/assets/styles/index.scss';
+import { useEffect } from 'react';
 const App = () => {
 
   return (
-    <div className='container'>
+    <div className="container">
       <Routes>
         <Route path="/" element={<Main />}></Route>
-        <Route path="/anchor" element={<Sub />}></Route>
-        <Route path="/manager" element={<Sub />}></Route>
+        <Route path="/anchor/*" element={<Sub />}></Route>
+        <Route path="/manager/*" element={<Sub />}></Route>
       </Routes>
     </div>
   );
